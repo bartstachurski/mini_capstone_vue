d793f1710d59b630ec32101fd747db4ef6734f31
bartstachurski <bart.stachurski@gmail.com>
2019-07-30 20:50:21 -0400

    adds image view to index page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="product in products">
+      <p>Name: {{ product.name }}</p>
+      <p>Price: {{ product.price }}</p>
+      <img v-bind:src="product.image_url" v-bind:alt="product.name"/>
+    </div>
   </div>
 </template>
 
@@ -8,13 +13,22 @@
 </style>
 
 <script>
+import axios from "axios"
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      products: []
     };
   },
-  created: function() {},
+  created: function() {
+    console.log("hello from the created!");
+    axios.get("/api/products").then(response => {
+      console.log(response.data);
+      this.products = response.data;
+    });
+  },
   methods: {}
 };
 </script>
